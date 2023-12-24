b072c08cce564c28e2490ef2533eb4db14635754
Cybernetics <mail@cybernetics.me>
2023-12-24 17:11:40 +0530

    small updates

--- resources/views/store/sections/quickview.blade.php
+++ resources/views/store/sections/quickview.blade.php
@@ -1,9 +1,9 @@
 <!-- Quick view -->
 <div class="modal fade custom-modal" id="quickViewModal" tabindex="-1" aria-labelledby="quickViewModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-        <div class="modal-content">              
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>       
-            <div class="modal-body">                
+        <div class="modal-content">
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div class="modal-body">
                 <form method="post" id="frmAddToCart" enctype="multipart/form-data">
                     @csrf
                     <input type="hidden" name="product_id" id="product_id" value="" />
@@ -15,11 +15,11 @@
                                 <div class="product-image-slider">
                                     <figure class="border-radius-10">
                                         <img src="" alt="" class="mainImg">
-                                    </figure>                               
+                                    </figure>
                                 </div>
                                 <!-- THUMBNAILS -->
                                 <div class="slider-nav-thumbnails pl-15 pr-15">
-                                    <div class="thumbImg"><img src="" alt=""></div>                              
+                                    <div class="thumbImg"><img src="" alt=""></div>
                                 </div>
                             </div>
                             <!-- End Gallery -->
@@ -28,7 +28,7 @@
                                 <div class="col-6 pdctTags fw-bold"></div>
                                 <div class="col-6 pdctStyle fw-bold"></div>
                                 <div class="col-6 pdctMaterial fw-bold"></div>
-                                <div class="col-12 in-stock fw-bold"></div>                            
+                                <div class="col-12 in-stock fw-bold"></div>
                             </div>
                             <div class="row mt-3">
                                 <div class="col text-center"><a href="" class="btn btn-sm btn-outline text-dark pdctUrl">Explore Details</a></div>
@@ -63,30 +63,38 @@
                                 <div class="row">
                                     <div class="col form-group attr-detail attr-color mb-15">
                                         <label class="mr-5">Color</label>
-                                        <select class="form-control form-control-sm color-filter" name="color"><option value="">--Choose Color--</option></select>
+                                        <select class="form-control form-control-sm color-filter" name="color">
+                                            <option value="">--Choose Color--</option>
+                                        </select>
                                     </div>
                                     <div class="col form-group attr-detail attr-size">
                                         <label class="mr-5">Size</label>
-                                        <select class="form-control form-control-sm size-filter" name="size"><option value="">--Choose Size--</option></select>
+                                        <select class="form-control form-control-sm size-filter" name="size">
+                                            <option value="">--Choose Size--</option>
+                                        </select>
                                     </div>
                                 </div>
                                 <div class="bt-1 border-color-1 mt-30 mb-30"></div>
                                 <div class="pdctPresc d-none">
                                     <div class="row">
-                                        <div class="col-12"><h5 class="primary-color">Prescription (If required)</h5></div>
+                                        <div class="col-12">
+                                            <h5 class="primary-color">Prescription (If required)</h5>
+                                        </div>
                                         <label class="fw-bold">RE</label>
                                         <div class="col-3"><input type="text" class="form-control form-control-sm" maxlength="6" placeholder="SPH" name="re_sph" /></div>
                                         <div class="col-3"><input type="text" class="form-control form-control-sm" maxlength="6" placeholder="CYL" name="re_cyl" /></div>
                                         <div class="col-3"><input type="text" class="form-control form-control-sm" maxlength="6" placeholder="AXIS" name="re_axis" /></div>
                                         <div class="col-3"><input type="text" class="form-control form-control-sm" maxlength="6" placeholder="ADD" name="re_add" /></div>
                                         <label class="fw-bold">LE</label>
-                                        <div class="col-3"><input type="text" class="form-control form-control-sm" maxlength="6" placeholder="SPH" name="le_sph"/></div>
+                                        <div class="col-3"><input type="text" class="form-control form-control-sm" maxlength="6" placeholder="SPH" name="le_sph" /></div>
                                         <div class="col-3"><input type="text" class="form-control form-control-sm" maxlength="6" placeholder="CYL" name="le_cyl" /></div>
                                         <div class="col-3"><input type="text" class="form-control form-control-sm" maxlength="6" placeholder="AXIS" name="le_axis" /></div>
                                         <div class="col-3"><input type="text" class="form-control form-control-sm" maxlength="6" placeholder="ADD" name="le_add" /></div>
                                     </div>
                                     <div class="row mt-3 mb-3">
-                                        <div class="col-12"><p class="primary-color text-center">OR</9></div>
+                                        <div class="col-12">
+                                            <p class="primary-color text-center">OR</9>
+                                        </div>
                                         <label>Attach Prescription</label>
                                         <div class="col"><input type="file" class="form-control form-control-sm" name="prescription" /></div>
                                     </div>
@@ -100,16 +108,16 @@
                                     </div>
                                     <div class="product-extra-link2">
                                         <button type="submit" class="button btn-submit button-add-to-cart">Add to Cart</button>
-                                        <a aria-label="Add To Wishlist" class="action-btn hover-up" href="javasript:void(0)"><i class="fi-rs-heart wishList" data-id=""></i></a>
-                                        <a aria-label="Compare" class="action-btn hover-up" href="javasript:void(0)"><i class="fi-rs-shuffle compare" data-id=""></i></a>
+                                        <a aria-label="Add To Wishlist" class="action-btn hover-up" href="javascript:void(0)"><i class="fi-rs-heart wishList" data-id=""></i></a>
+                                        <a aria-label="Compare" class="action-btn hover-up" href="javascript:void(0)"><i class="fi-rs-shuffle compare" data-id=""></i></a>
                                     </div>
-                                </div>                            
+                                </div>
                             </div>
                             <!-- Detail Info -->
                         </div>
                     </div>
-                </form>                
-            </div>        
+                </form>
+            </div>
         </div>
     </div>
 </div>
